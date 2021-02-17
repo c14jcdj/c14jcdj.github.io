518fd09350f1b83eacec76e093f05ed817d4615f
Chermaine Zimmerman <chermaine.zimmerma@rakuten.com>
2021-02-16 19:57:04 -0800

    wip

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 
-$(document).ready(function(){
-	grantEntry()
-})
+// $(document).ready(function(){
+// 	grantEntry()
+// })
 
-function grantEntry(){
-	var ssPassword = 'i'
-	$('form').on('submit', function(e){
-		e.preventDefault()
-		var code = $('input[name="code"]').val()
-		if(code == ssPassword){
-			$('#main_background').fadeIn(3000, displayMainPage)
-		}
-	})	
-}
+// function grantEntry(){
+// 	var ssPassword = 'i'
+// 	$('form').on('submit', function(e){
+// 		e.preventDefault()
+// 		var code = $('input[name="code"]').val()
+// 		if(code == ssPassword){
+// 			$('#main_background').fadeIn(3000, displayMainPage)
+// 		}
+// 	})	
+// }
 
-function displayMainPage(){
+// function displayMainPage(){
 
-	function showElements(){
-		$('#message').slideDown(1000)
-		timeoutID2 = window.setTimeout(showSection, 1000);
+// 	function showElements(){
+// 		$('#message').slideDown(1000)
+// 		timeoutID2 = window.setTimeout(showSection, 1000);
 		
-	}
-	function showSection(){
-		$('#one').show().addClass('animated slideInLeft')
-		$('#two').show().addClass('animated slideInRight')
-		$('#three').show().addClass('animated slideInLeft')
-		$('#four').show().addClass('animated slideInRight')
-	}
+// 	}
+// 	function showSection(){
+// 		$('#one').show().addClass('animated slideInLeft')
+// 		$('#two').show().addClass('animated slideInRight')
+// 		$('#three').show().addClass('animated slideInLeft')
+// 		$('#four').show().addClass('animated slideInRight')
+// 	}
 
-	timeoutID = window.setTimeout(showElements, 2000);
-}
+// 	timeoutID = window.setTimeout(showElements, 2000);
+// }
+alert('yo')
